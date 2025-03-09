7902c2056cc411814ef3c55a8a49e6244e143dab
Pham2uanghuy <2uanghuy2k3@gmail.com>
2025-03-09 22:28:40 +0700

    # thay đổi - cấu hình lại môi trường từ .properties sang .env
    # cách kiểm tra
    - tạo file .env theo mẫu của .env.example

--- src/main/java/com/example/demo/TravelingApplication.java
+++ src/main/java/com/example/demo/TravelingApplication.java
@@ -2,11 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+import java.io.File;
 
 @SpringBootApplication
 public class TravelingApplication {
 
 	public static void main(String[] args) {
+		// Kiểm tra nếu .env tồn tại thì mới load
+		File envFile = new File(".env");
+		if (envFile.exists()) {
+			Dotenv dotenv = Dotenv.load();
+			dotenv.entries().forEach(entry ->
+					System.setProperty(entry.getKey(), entry.getValue())
+			);
+		}
 		SpringApplication.run(TravelingApplication.class, args);
 	}
 
